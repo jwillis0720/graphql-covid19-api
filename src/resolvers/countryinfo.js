454797fbd96e7bf8d17006b7ac8c748bb487dc86
Jordan Willis <jordanwillis@Jordans-MacBook-Pro.local>
2020-04-28 01:49:52 -0500

    test for all countries

--- src/resolvers/countryinfo.js
+++ src/resolvers/countryinfo.js
@@ -1,7 +1,11 @@
 const countryInfoObject = {
   population: async (countryInfo, _, { dataSources }) => {
     const response = await dataSources.csv.getCountryInfo(countryInfo);
-    console.log(response);
+    // console.log(response);
+    if (response === undefined) {
+      console.log(countryInfo.iso3, 'has no population?');
+      return null;
+    }
     return response['pop_est'];
   },
 
--- tests/queries.test.js
+++ tests/queries.test.js
@@ -3,20 +3,46 @@ import { gql } from 'apollo-boost';
 import client from './utils/getClient';
 import 'regenerator-runtime/runtime.js';
 // const client = getClient();
-describe('the Queries that can be performed on TODO and USER type', () => {
-  it("should be able to see author's profile without sensitive info being displayed", async () => {
+describe('The all countries query', () => {
+  it('should return a dataray of all countries', async () => {
     const userQuery = gql`
       query {
         AllCountries {
+          name
           date
           datereadable
+          cummulativeCases
+          todayCases
+          yesterdayCases
+          cummulativeDeaths
+          todayDeaths
+          yesterdayDeaths
+          recovered
+          active
+          critical
+          casesPerOneMillion
+          deathsPerOneMillion
+          tests
+          testsPerOneMillion
+          continent
+          info {
+            id
+            iso2
+            iso3
+            lat
+            long
+            flag
+            population
+          }
         }
       }
     `;
     const { data } = await client.query({
       query: userQuery,
     });
-    console.log(data);
+    // console.log(data);
+
     expect(data.AllCountries.length).toBeGreaterThan(200);
+    //can have multiple expect statements
   });
 });
